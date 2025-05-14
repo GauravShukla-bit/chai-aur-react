a70a798d0825ed441822f2d6f3e37ad999313e99
GauravShukla-bit <gshukla925@gmail.com>
2025-05-14 18:49:38 +0530

    create react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>chai aur code | Gaurav</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  
+
+  return (
+   <h1>Chai aur React with Vite | Gaurav</h1>
+  )
+}
+
+export default App
