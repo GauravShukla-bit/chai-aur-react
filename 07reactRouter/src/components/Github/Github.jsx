8530792cd53ad53c463f285b7ec1fe3ca286dd13
GauravShukla-bit <gshukla925@gmail.com>
2025-05-30 18:24:25 +0530

    React router crash course

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+  const data = useLoaderData()
+
+  // const [data, setUserData]= useState([])
+
+  // useEffect (()=>{
+  //   fetch('https://api.github.com/users/hiteshchoudhary')
+  //   .then(response => response.json())
+  //   .then(data => setUserData(data))
+  // },[])  
+  return (
+     <div className="text-center m-4 bg-grey-600 text-black p-4 text-3xl">Github Followers: {data.followers} 
+     <img src={data.avatar_url} alt="Git picture" width={300}/>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async() => {
+  const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+    
+}
